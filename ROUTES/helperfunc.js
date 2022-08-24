53b90d2edf3b4d879707e35621db6b7995ccdc03
Nishantphule <nishantphule12@gmail.com>
2022-08-24 23:52:44 +0530

    objectId

--- ROUTES/helperfunc.js
+++ ROUTES/helperfunc.js
@@ -1,16 +1,17 @@
+import { ObjectId } from 'mongodb';
 import { client } from '../index.js';
 
 export async function updateMovieById(id, data) {
-    return await client.db("Nishant").collection("movies").updateOne({ _id: id }, { $set: data });
+    return await client.db("Nishant").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function createMovies(data) {
     return await client.db("Nishant").collection("movies").insertMany(data);
 }
 export async function deleteMovieById(id) {
-    return await client.db("Nishant").collection("movies").deleteOne({ _id: id });
+    return await client.db("Nishant").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function getMovieById(id) {
-    return await client.db("Nishant").collection("movies").findOne({  _id: id });
+    return await client.db("Nishant").collection("movies").findOne({  _id: ObjectId(id) });
 }
 export async function getAllMovies(req) {
     return await client
